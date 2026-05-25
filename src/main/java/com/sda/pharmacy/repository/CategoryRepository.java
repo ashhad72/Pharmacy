3fc19bbc427e25968f40d824a6b4d71b9a844d24
Ahmed Nadeem <ahmedakhter05@gmail.com>
2026-05-25 14:19:38 +0500

    Added Category repo and supplier repo classes and tested the prevoius code

---
+++ src/main/java/com/sda/pharmacy/repository/CategoryRepository.java
@@ -0,0 +1,18 @@
+package com.sda.pharmacy.repository;
+
+import com.sda.pharmacy.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+    Optional<Category> findByCategoryName(String categoryName);
+
+    List<Category> findByCategoryNameContainingIgnoreCase(String keyword);
+
+    boolean existsByCategoryName(String categoryName);
+}
